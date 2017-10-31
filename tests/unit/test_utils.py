7384b98998ae57c4b9db98ca41d84011b636f196
Andreas Maier <andreas.r.maier@gmx.de>
2017-10-31 16:20:27 +0100

    Fixed flake8 errors in test module; Added pylint support; disabled builddoc in Travis for now.
    Signed-off-by: Andreas Maier <andreas.r.maier@gmx.de>

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -22,13 +22,6 @@ All tests for ownmoviedb/utils.py module.
 
 from __future__ import absolute_import, print_function
 
-import re
-import sys
-import subprocess
-import os.path
-import fnmatch
-import time
-
 import pytest
 
 from ownmoviedb.utils import ParseComplexTitle
@@ -41,176 +34,151 @@ from ownmoviedb.utils import ParseComplexTitle
           "year": None,
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (1999)",
          {"title": "A",
           "year": "1999",
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (1999) (2000)",
          {"title": "A (1999)",
           "year": "2000",
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (1999) B",
          {"title": "A B",
           "year": "1999",
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (1999) 2000",
          {"title": "A 2000",
           "year": "1999",
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (199)",
          {"title": "A (199)",
           "year": None,
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (199x)",
          {"title": "A (199x)",
           "year": None,
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A (19990)",
          {"title": "A (19990)",
           "year": None,
           "series_title": None,
           "episode_id": None,
-          "episode_title": None}
-        ),
+          "episode_title": None}),
         ("A - 1 - B",
          {"title": "A - 1 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "1",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A - 01 - B",
          {"title": "A - 01 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "01",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A - 001 - B",
          {"title": "A - 001 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "001",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A - 123 - B",
          {"title": "A - 123 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "123",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A - 123a - B",
          {"title": "A - 123a - B",
           "year": None,
           "series_title": "A",
           "episode_id": "123a",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A - 12.13 - B",
          {"title": "A - 12.13 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "12.13",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Folge 12 - B",
          {"title": "A, Folge 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Folge 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("12, Folge 13 - 14",
          {"title": "12, Folge 13 - 14",
           "year": None,
           "series_title": "12",
           "episode_id": "Folge 13",
-          "episode_title": "14"}
-        ),
+          "episode_title": "14"}),
         ("A, Doppelfolge 12 - B",
          {"title": "A, Doppelfolge 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Doppelfolge 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Teil 12 - B",
          {"title": "A, Teil 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Teil 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Part 12 - B",
          {"title": "A, Part 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Part 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, part 12 - B",
          {"title": "A, part 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "part 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Buch 12 - B",
          {"title": "A, Buch 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Buch 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Book 12 - B",
          {"title": "A, Book 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Book 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, book 12 - B",
          {"title": "A, book 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "book 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Episode 12 - B",
          {"title": "A, Episode 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Episode 12",
-          "episode_title": "B"}
-        ),
+          "episode_title": "B"}),
         ("A, Film 12 - B",
          {"title": "A, Film 12 - B",
           "year": None,
           "series_title": "A",
           "episode_id": "Film 12",
-          "episode_title": "B"}
-        )
+          "episode_title": "B"}),
     ]
 )
 def test_one_ParseComplexTitle(complex_title, exp_rv):
